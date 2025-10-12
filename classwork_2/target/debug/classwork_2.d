e3f43d97a3f9637780e93e41ce9d9c38e6cd79df
JoshuaPAU1 <joshua.adegoke20@gmail.com>
2025-10-12 13:43:08 +0100

    Updated classwork 2 files

---
+++ classwork_2/target/debug/classwork_2.d
@@ -0,0 +1,1 @@
+/home/joshu/csc308/classwork_2/target/debug/classwork_2: /home/joshu/csc308/classwork_2/src/main.rs
---
+++ classwork_2/target/debug/deps/classwork_2-c87d7f6df65ab6a0.d
@@ -0,0 +1,5 @@
+/home/joshu/csc308/classwork_2/target/debug/deps/classwork_2-c87d7f6df65ab6a0.d: src/main.rs
+
+/home/joshu/csc308/classwork_2/target/debug/deps/classwork_2-c87d7f6df65ab6a0: src/main.rs
+
+src/main.rs:
