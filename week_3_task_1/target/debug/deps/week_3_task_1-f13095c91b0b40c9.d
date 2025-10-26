899e6c8e71d5b8b2427257a4386fb8bd9d500c87
JoshuaPAU1 <joshua.adegoke20@gmail.com>
2025-10-26 13:52:39 +0100

    Created first task files

---
+++ week_3_task_1/target/debug/deps/week_3_task_1-f13095c91b0b40c9.d
@@ -0,0 +1,5 @@
+/home/joshu/csc308/week_3_task_1/target/debug/deps/week_3_task_1-f13095c91b0b40c9.d: src/main.rs
+
+/home/joshu/csc308/week_3_task_1/target/debug/deps/week_3_task_1-f13095c91b0b40c9: src/main.rs
+
+src/main.rs:
---
+++ week_3_task_1/target/debug/week_3_task_1.d
@@ -0,0 +1,1 @@
+/home/joshu/csc308/week_3_task_1/target/debug/week_3_task_1: /home/joshu/csc308/week_3_task_1/src/main.rs
