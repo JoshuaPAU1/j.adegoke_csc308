cc06e0f7a9135c4f7a3a1c4eecb4e945558b4f66
JoshuaPAU1 <joshua.adegoke20@gmail.com>
2025-10-26 13:54:40 +0100

    Created second task files

---
+++ week_3_task_2/target/debug/deps/week_3_task_2-a2f3c3b20a8fdff3.d
@@ -0,0 +1,5 @@
+/home/joshu/csc308/week_3_task_2/target/debug/deps/week_3_task_2-a2f3c3b20a8fdff3.d: src/main.rs
+
+/home/joshu/csc308/week_3_task_2/target/debug/deps/week_3_task_2-a2f3c3b20a8fdff3: src/main.rs
+
+src/main.rs:
---
+++ week_3_task_2/target/debug/week_3_task_2.d
@@ -0,0 +1,1 @@
+/home/joshu/csc308/week_3_task_2/target/debug/week_3_task_2: /home/joshu/csc308/week_3_task_2/src/main.rs
