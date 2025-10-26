8b530f10b347e61f148d5592ae6f9914993f1425
JoshuaPAU1 <joshua.adegoke20@gmail.com>
2025-10-26 13:56:32 +0100

    Created fourth task files

---
+++ week_3_task_4/target/debug/deps/week_3_task_4-7207d2f5fef5d35e.d
@@ -0,0 +1,5 @@
+/home/joshu/csc308/week_3_task_4/target/debug/deps/week_3_task_4-7207d2f5fef5d35e.d: src/main.rs
+
+/home/joshu/csc308/week_3_task_4/target/debug/deps/week_3_task_4-7207d2f5fef5d35e: src/main.rs
+
+src/main.rs:
---
+++ week_3_task_4/target/debug/week_3_task_4.d
@@ -0,0 +1,1 @@
+/home/joshu/csc308/week_3_task_4/target/debug/week_3_task_4: /home/joshu/csc308/week_3_task_4/src/main.rs
