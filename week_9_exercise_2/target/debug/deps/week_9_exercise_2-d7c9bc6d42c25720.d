fd260e57d14183e0f831674e531411812584d9b6
JoshuaPAU1 <joshua.adegoke20@gmail.com>
2025-12-07 21:38:04 +0100

    Created exercise two files

---
+++ week_9_exercise_2/target/debug/deps/week_9_exercise_2-d7c9bc6d42c25720.d
@@ -0,0 +1,5 @@
+/home/joshu/csc308/week_9_exercise_2/target/debug/deps/week_9_exercise_2-d7c9bc6d42c25720.d: src/main.rs
+
+/home/joshu/csc308/week_9_exercise_2/target/debug/deps/week_9_exercise_2-d7c9bc6d42c25720: src/main.rs
+
+src/main.rs:
---
+++ week_9_exercise_2/target/debug/week_9_exercise_2.d
@@ -0,0 +1,1 @@
+/home/joshu/csc308/week_9_exercise_2/target/debug/week_9_exercise_2: /home/joshu/csc308/week_9_exercise_2/src/main.rs
